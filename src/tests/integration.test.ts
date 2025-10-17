6c8c7385321642a888256df6762053eaad1d285f
Eryck Assis <eng.assis.dev@gmail.com>
2025-10-16 22:18:03 -0300

    test: add integration test for button initialization on DOM load

--- src/tests/integration.test.ts
+++ src/tests/integration.test.ts
@@ -7,4 +7,14 @@ describe("Integration Test", () => {
       <output id="x-pos"></output>
       <output id="y-pos"></output>`;
   });
+  it("should be initialize all buttons correctly on DOM Load", () => {
+    const event = new Event("DOMContentLoaded");
+    document.dispatchEvent(event);
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+      button.click();
+      expect(button).toHaveClass("following");
+      expect(button.textContent).toBe("Unfollow");
+    });
+  });
 });
